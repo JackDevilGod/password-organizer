2be210eff4bdc10884b12506926489b333f58362
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2024-05-03 14:57:40 +0200

    added a simple password generator

--- password generator.py
+++ password generator.py
@@ -24,11 +24,34 @@ def variable_input(var_type: str, texts=""):
 
 
 def main():
-    length: int = int(variable_input("num", "length of password"))
     password: str = ""
 
+    # settings
+    length: int = 20
+    symbols: bool = True
+    upper_letters: bool = True
+    lower_letters: bool = True
+    numbers: bool = True
+
+    symbol_list: list[int] = ([_ for _ in range(33, 47)] + [_ for _ in range(58, 65)] +
+                              [_ for _ in range(91, 97)] + [_ for _ in range(123, 127)])
+    upper_letters_list: list[int] = [_ for _ in range(65, 91)]
+    lower_letter_list: list[int] = [_ for _ in range(97, 123)]
+    numbers_list: list[int] = [_ for _ in range(48, 58)]
+
+    possible_list: list[int] = list()
+
+    if symbols:
+        possible_list += symbol_list
+    if upper_letters:
+        possible_list += upper_letters_list
+    if lower_letters:
+        possible_list += lower_letter_list
+    if numbers:
+        possible_list += numbers_list
+
     for _ in range(length):
-        password += chr(random.randint(33, 126))
+        password += chr(possible_list[random.randint(0, len(possible_list) - 1)])
 
     print(password)
 
